cdcb2905dac31bf21de74012fc492c98262dfb52
Mauricio Cox <coxmau77@gmail.com>
2024-09-09 01:06:22 -0300

    Trabajamos en el componente header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {},
-  },
+  theme: {},
   plugins: [],
+  darkMode: [
+    "variant",
+    [
+      "@media (prefers-color-scheme: dark) { &:not(.light *) }",
+      "&:is(.dark *)",
+    ],
+  ],
 };
