a73db249c9cd2bb538d5c3a2a8f478375752a7f8
Mauricio Cox <coxmau77@gmail.com>
2024-09-09 12:29:43 -0300

    se hizo Deploy en Vecel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  // Integraciones de Astro con Tailwind y React
   integrations: [tailwind(), react()],
+
+  // Configuraciones adicionales si las necesitas
+  build: {
+    // Ejemplo de opciones adicionales (opcional)
+    // client: './dist/client', // Cambia la ubicación de los archivos del cliente
+    // server: './dist/server', // Cambia la ubicación de los archivos del servidor
+    // assets: './dist/assets'  // Carpeta para activos (imágenes, fuentes, etc.)
+  },
 });
